6d524ddc0a3ed311f3b70cac5073bb1cb07fa0b6
Miguel González Pascual <miguelgp789@gmail.com>
2025-11-19 13:07:53 +0100

    feat: add CardNav component and integrate routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,87 @@
 import './App.css'
 import FloatingLines from './components/FloatingLines'
+import logo from './assets/react.svg'
+import CardNav from './components/CardNav'
 
 function App() {
+    const items = [
+        {
+            label: 'Sobre Mi',
+            bgColor: '#0D0716',
+            textColor: '#fff',
+            links: [
+                {
+                    label: 'Sobre Mi',
+                    href: '#about',
+                    ariaLabel: 'Ir a la sección Sobre Mi'
+                }
+            ]
+        },
+		{
+            label: 'Habilidades',
+            bgColor: '#170D27',
+            textColor: '#fff',
+            links: [
+                {
+                    label: 'Habilidades',
+                    href: '#skills',
+                    ariaLabel: 'Ir a la sección Habilidades'
+                }
+            ]
+        },
+        {
+            label: 'Proyectos',
+            bgColor: '#271E37',
+            textColor: '#fff',
+            links: [
+                {
+                    label: 'Ver Proyectos',
+                    href: '#projects',
+                    ariaLabel: 'Ir a la sección Proyectos'
+                }
+            ]
+        },
+        
+    ]
+    return (
+        <>
+            <div
+                style={{
+                    width: '100vw',
+                    height: '100vh',
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    backgroundColor: '#000',
+                    overflow: 'hidden'
+                }}
+            >
+                <FloatingLines
+                    enabledWaves={['top', 'middle', 'bottom']}
+                    lineCount={[10, 10, 10]}
+                    lineDistance={[8, 6, 4]}
+                    bendRadius={5.0}
+                    bendStrength={-0.5}
+                    interactive={false}
+                    parallax={true}
+                />
+            </div>
+            <CardNav
+                logo={logo}
+                logoAlt="Company Logo"
+                items={items}
+                baseColor="#fff"
+                menuColor="#000"
+                ease="power3.out"
+            />
 
-  return (
-    <>
-	<div style={{ 
-    width: '100vw', 
-    height: '100vh', 
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    backgroundColor: '#000',
-    overflow: 'hidden'
-  }}>
-  <FloatingLines 
-    enabledWaves={['top', 'middle', 'bottom']}
-    // Array - specify line count per wave; Number - same count for all waves
-    lineCount={[10, 10, 10]}
-    // Array - specify line distance per wave; Number - same distance for all waves
-    lineDistance={[8, 6, 4]}
-    bendRadius={5.0}
-    bendStrength={-0.5}
-    interactive={false}
-    parallax={true}
-  />
-	</div>
-    </>
-  )
+			// Componente de Sobre Mi
+
+			// Componente de Habilidades
+
+			// Componente de Proyectos
+        </>
+    )
 }
 
 export default App

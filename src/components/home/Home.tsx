481a8a8d6d6f9892d0907d8cf89e0731853a9917
Miguel González Pascual <miguelgp789@gmail.com>
2025-11-20 11:51:31 +0100

    feat: agregar indicador de scroll en el componente Home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -36,6 +36,16 @@ const Home = () => {
                     className="w-[190%] max-w-[1100px] h-auto object-contain md:translate-x-20 lg:translate-x-75"
                 />
             </div>
+
+            {/* Indicador de scroll */}
+            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-3">
+                <p className="text-gray-400 text-sm tracking-wider">
+                    Desliza para explorar
+                </p>
+                <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center p-2">
+                    <div className="w-1 h-2 bg-gray-400 rounded-full animate-scroll"></div>
+                </div>
+            </div>
         </section>
     )
 }
